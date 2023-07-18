0dda0a43d1fb2042da08f20cdd4c238ba001ab7e
redmango1446@gmail.com <redmango1446>
2023-07-18 23:00:07 +0900

    feat: Banker타입 추상화 프로토콜 및 BankerWork Enum타입 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
@@ -6,8 +6,33 @@
 //
 import Foundation
 
-struct Banker {
-    private let workTime: Double = 0.7
+protocol Bankerable {
+    var work: BankerWork { get }
+    
+    mutating func work(for customer: Customer)
+    func notifyWorkTime() -> Double
+}
+
+enum BankerWork {
+    case deposit
+    case loans
+    
+    var time: Double {
+        switch self {
+        case .deposit:
+            return 0.7
+        case .loans:
+            return 1.1
+        }
+    }
+}
+
+struct Banker: Bankerable {
+    var work: BankerWork
+    
+    init(work: BankerWork) {
+        self.work = work
+    }
     
     func work(for customer: Customer) {
         guard let queueNumber = customer.queueNumber else {
@@ -15,11 +40,11 @@ struct Banker {
         }
         
         print("\(queueNumber)번 고객 업무 시작")
-        Thread.sleep(forTimeInterval: workTime)
+        Thread.sleep(forTimeInterval: work.time)
         print("\(queueNumber)번 고객 업무 종료")
     }
     
     func notifyWorkTime() -> Double {
-        return workTime
+        return work.time
     }
 }
