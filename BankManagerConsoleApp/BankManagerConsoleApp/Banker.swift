7f908e4e81dc3c5db13f8b7baedfc8f4c6cbcd40
redmango1446@gmail.com <redmango1446>
2023-07-18 23:11:26 +0900

    refactor: BankWork 타입 명 -> BankTask로 변경 및 파일 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
@@ -7,31 +7,17 @@
 import Foundation
 
 protocol Bankerable {
-    var work: BankerWork { get }
+    var task: BankTask { get }
     
     mutating func work(for customer: Customer)
     func notifyWorkTime() -> Double
 }
 
-enum BankerWork {
-    case deposit
-    case loans
-    
-    var time: Double {
-        switch self {
-        case .deposit:
-            return 0.7
-        case .loans:
-            return 1.1
-        }
-    }
-}
-
 struct Banker: Bankerable {
-    var work: BankerWork
+    var task: BankTask
     
-    init(work: BankerWork) {
-        self.work = work
+    init(task: BankTask) {
+        self.task = task
     }
     
     func work(for customer: Customer) {
@@ -40,11 +26,11 @@ struct Banker: Bankerable {
         }
         
         print("\(queueNumber)번 고객 업무 시작")
-        Thread.sleep(forTimeInterval: work.time)
+        Thread.sleep(forTimeInterval: task.time)
         print("\(queueNumber)번 고객 업무 종료")
     }
     
     func notifyWorkTime() -> Double {
-        return work.time
+        return task.time
     }
 }
